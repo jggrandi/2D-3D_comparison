bd2acc231a577a482596af3437055e824e5e2302
Jeronimo Grandi <jggrandi@gmail.com>
2013-09-23 14:57:36 -0300

    Problemas de alocação de memória resolvidos

--- Makefile
+++ Makefile
@@ -6,14 +6,19 @@ WARNINGOFF = -w
 PROG = ../bin/2d_3d_comparison
 
 SRC = main.cpp 
+OPENCV = `pkg-config opencv --libs --cflags`
 
 MY_INCLUDE_HANDLE3DDATASETS =-I/home/jeronimo/Dropbox/UFRGS/Mestrado/Devel/my_libs/Handle3DDataset/src 
 MY_LIB_PATCH_HANDLE3DDATASETS = -L/home/jeronimo/Dropbox/UFRGS/Mestrado/Devel/my_libs/Handle3DDataset/bin
 
+MY_INCLUDE_QUALITYASSESSMENT =-I/home/jeronimo/Dropbox/UFRGS/Mestrado/Devel/my_libs/QualityAssessment/src 
+MY_LIB_QUALITYASSESSMENT = -lqualityassessment
+MY_LIB_PATCH_QUALITYASSESSMENT = -L/home/jeronimo/Dropbox/UFRGS/Mestrado/Devel/my_libs/QualityAssessment/bin
+MY_R_PATCH_QUALITYASSESSMENT = -Wl,-R/home/jeronimo/Dropbox/UFRGS/Mestrado/Devel/my_libs/QualityAssessment/bin
+
 
 $(PROG): $(SRC)
-	 $(CC) $(CFLAGS) $(CDEBUG) $(WARNINGOFF) -o $(PROG) $(SRC) $(MY_INCLUDE_HANDLE3DDATASETS) $(MY_LIB_HANDLE3DDATASETS)  $(MY_LIB_PATCH_HANDLE3DDATASETS)
-
+	 $(CC) $(CFLAGS) $(CDEBUG) -o $(PROG) $(SRC) $(OPENCV) $(MY_INCLUDE_HANDLE3DDATASETS) $(MY_INCLUDE_QUALITYASSESSMENT) $(MY_LIB_HANDLE3DDATASETS) $(MY_LIB_QUALITYASSESSMENT) $(MY_LIB_PATCH_HANDLE3DDATASETS) $(MY_LIB_PATCH_QUALITYASSESSMENT) $(MY_R_PATCH_HANDLE3DDATASETS) $(MY_R_PATCH_QUALITYASSESSMENT)
 
 clean:
 	rm -f $(PROG)
--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include <cstdlib>
+#include <cstdio>
+
 #include <handle3ddataset.h>
-
+#include <qualityassessment.h>
 #include <opencv2/core/core.hpp>  
-
-#include <fstream>
 #include <ctime>
 
 using namespace cv;
@@ -11,116 +11,82 @@ using namespace cv;
 #define ijn(a,b,n) ((a)*(n))+b
 #define V false
 
-#define KERNEL 1
+#define KERNEL 4
 #define PBASE KERNEL*2+1
 #define OFFSET KERNEL
 
 
-void buildImagePlanes(int d, int w, int h, int img_w)
+void buidImagePlanes(int d, int w, int h, int resW, char **data1, int diag_type, Mat &t)
 {
-	if(V)printf("________AXIAL\n");
+	//char *subVol= new char[PBASE*PBASE];
+	char *subVol = (char*)malloc(sizeof(char*)* PBASE*PBASE);//sub imagens
+	int iC1,jC1,jC2;
 	for(int i = 0; i < PBASE; i++)
 	{
 		for(int j = 0; j < PBASE; j++)
 		{
-			if(V)printf("[%d,%d] ",d-KERNEL+1,ijn(w-KERNEL+i,h-KERNEL+j,img_w));
-		}
-		if(V)printf("\n");	
-	}
-	if(V)printf("\n");
+			switch(diag_type)
+			{
+				case 0:
+					iC1 = d-KERNEL+1;
+					jC1 = w-KERNEL+i;
+					jC2 = h-KERNEL+j;
+				break;
+				case 1:
+					iC1 = d-KERNEL+i;
+					jC1 = w-KERNEL+j;
+					jC2 = h-KERNEL+j;					
+				break;
+				case 2:
+					iC1 = d-KERNEL+i;
+					jC1 = w-KERNEL+j;
+					jC2 = h-KERNEL-j-1;				
+				break;
+				case 3:
+					iC1 = d-KERNEL+j;
+					jC1 = w-KERNEL+1;
+					jC2 = h-KERNEL+i;				
+				break;
+				case 4:
+					iC1 = d-KERNEL+i;
+					jC1 = w-KERNEL+j;
+					jC2 = h-KERNEL+i;				
+				break;
+				case 5:
+					iC1 = d-KERNEL+(PBASE-i-1);
+					jC1 = w-KERNEL+j;
+					jC2 = h-KERNEL+(PBASE-i-1);				
+				break;
+				case 6:
+					iC1 = d-KERNEL+j;
+					jC1 = w-KERNEL+i;
+					jC2 = h-KERNEL+1;				
+				break;
+				case 7:
+					iC1 = d-KERNEL+i;
+					jC1 = w-KERNEL+i;
+					jC2 = h-KERNEL+j;				
+				break;
+				case 8:
+					iC1 = d-KERNEL+(PBASE-i-1);
+					jC1 = w-KERNEL+(PBASE-i-1);
+					jC2 = h-KERNEL+j;				
+				break;			
+				default:
+				break;
+			}		
+			subVol[ijn(i,j,PBASE)] = data1[iC1][ijn(jC1,jC2,resW)];
+		}			
+	}		
 
-	if(V)printf("________DIAGONAL PRINCIPAL AXIAL\n");
-	for(int i = 0; i < PBASE; i++)
-	{
-		for(int j = 0; j < PBASE; j++)
-		{
-			if(V)printf("[%d,%d] ",d-KERNEL+i,ijn(w-KERNEL+j,h-KERNEL+j,img_w));
-		}
-		if(V)printf("\n");	
-	}
-	if(V)printf("\n");
-
-	if(V)printf("______DIAGONAL SECUNDARIA AXIAL\n");
-	for(int i = 0; i < PBASE; i++)
-	{
-		for (int j = 0; j < PBASE; j++)
-		{
-			if(V)printf("[%d,%d] ",d-KERNEL+i,ijn(w-KERNEL+j,h-KERNEL+PBASE-j-1,img_w));	
-		}
-		if(V)printf("\n");
-	}
-	if(V)printf("\n");
-
-	if(V)printf("________CORONAL\n");
-	for(int i = 0; i < PBASE; i++)
-	{
-		for(int j = 0; j < PBASE; j++)
-		{
-			if(V)printf("[%d,%d] ",d-KERNEL+j,ijn(w-KERNEL+1,h-KERNEL+i,img_w));
-		}
-		if(V)printf("\n");	
-	}
-	if(V)printf("\n");
-
-	if(V)printf("_______DIAGONAL PRINCIPAL CORONAL\n");
-	for(int i = 0; i < PBASE; i++)
-	{
-		for(int j = 0; j < PBASE; j++)
-		{
-			if(V)printf("[%d,%d] ",d-KERNEL+i,ijn(w-KERNEL+j,h-KERNEL+i,img_w));
-		}
-		if(V)printf("\n");	
-	}
-	if(V)printf("\n");
-
-	if(V)printf("___#___NEWDIAGONAL SECUNDARIA CORONAL\n");
-	for(int i = 0; i < PBASE; i++)
-	{
-		for(int j = 0; j < PBASE; j++)
-		{
-			if(V)printf("[%d,%d] ",d-KERNEL+(PBASE-i-1),ijn(w-KERNEL+j,h-KERNEL+(PBASE-i-1),img_w));
-		}
-		if(V)printf("\n");	
-	}
-	if(V)printf("\n");
-
-	if(V)printf("________SAGITTAL\n");
-	for(int i = 0; i < PBASE; i++)
-	{
-		for(int j = 0; j < PBASE; j++)
-		{
-			if(V)printf("[%d,%d] ",d-KERNEL+j,ijn(w-KERNEL+i,h-KERNEL+1,img_w));
-		}
-		if(V)printf("\n");	
-	}
-	if(V)printf("\n");
-
-	if(V)printf("________DIAGONAL PRINCIPAL SAGITAL\n");
-	for(int i = 0; i < PBASE; i++)
-	{
-		for(int j = 0; j < PBASE; j++)
-		{
-			if(V)printf("[%d,%d] ",d-KERNEL+i,ijn(w-KERNEL+i,h-KERNEL+j,img_w));
-		}
-	if(V)	printf("\n");	
-	}
-	if(V)printf("\n");
-
-	if(V)printf("___#___NEWDIAGONAL SECUNDARIA SAGITAL\n");
-	for(int i = 0; i < PBASE; i++)
-	{
-		for(int j = 0; j < PBASE; j++)
-		{
-			if(V)printf("[%d,%d] ",d-KERNEL+(PBASE-i-1),ijn(w-KERNEL+(PBASE-i-1),h-KERNEL+j,img_w));
-		}
-		if(V)printf("\n");	
-	}
-	if(V)printf("\n");
+	Mat slice(PBASE,PBASE,CV_8SC1,subVol);
+	t = slice.clone();
+	free(subVol);
+	subVol=0;	
 }
 
 int main(int argc, char **argv)
 {
-	int option_index = 0;
 
 	DATAINFO img1Info;
 
@@ -144,91 +110,76 @@ int main(int argc, char **argv)
 	d1.loadFile();
 
 	char **data1 = d1.getDataset();
-	char *data2 = data1[0];
+	char *data2 = data1[KERNEL];
 
-	// printf("#########Img2D\n");
-	// for (int i = 0; i < img1Info.resWidth; ++i)
-	// {
-	// 	for (int j = 0; j < img1Info.resHeight; ++j)
-	// 	{
-	// 		printf("%d ",data2[i*img1Info.resWidth+j]);			
-	// 	}
-	// 	printf("\n");
-	// }
-	// printf("\n\n\n");
+	char *subImg = (char*)malloc(sizeof(char*)* PBASE*PBASE);//sub imagens
+	QualityAssessment qualAssess;
+	Scalar mpsnrV;
 
-	char *subImg;
-	subImg = (char*)malloc(sizeof(char)* PBASE*PBASE);//sub imagens
-	
-	
-	// for (int i = offset; i < img1Info.resWidth-offset; i++) //percorre imagem pixel //coluna
-	// {
-	// 	for (int j = offset; j < img1Info.resHeight-offset; j++) //a pixel //linha
-	// 	{	
-	// 		for(int ii=0; ii < pBase; ii++)
-	// 		{
-	// 			for(int jj=0; jj < pBase; jj++)
-	// 			{
-	// 				int pData2 = ijn(i-kernel+ii, j-kernel+jj ,img1Info.resWidth);
-	// 				subImg[ijn(ii, jj, pBase)] = data2[pData2];
-	// 				printf("%d ", (int)subImg[ijn(ii, jj, pBase)] ); 
-	// 			}
-	// 			printf("\n");
-	// 		}
-	// 		printf("\n\n\n");
-	// 	}
-	// }	
-
-//****************************
-	printf("############### PERCORRE VOLUME\n");
-
-
-
+	int count=0;
+	int count2=0;
+	int count3=0;
 	clock_t start = clock();
-	
-	// // slice por slice comum
-	// for (int k = img1Info.initStack; k < img1Info.endStack; ++k)
-	// {
-	// 	for (int i = 0; i < img1Info.resWidth; ++i)
-	// 	{
-	// 		for (int j = 0; j < img1Info.resHeight; ++j)
-	// 		{
-	// 			if(v)printf("[%d][%d] ",k,i*img1Info.resWidth+j);
-	// 		}
-	// 		if(v)printf("\n");
-	// 	}
-	// 	if(v)printf("\n\n");
-	// }
-
-
-	for (int d = OFFSET; d < img1Info.resDepth-OFFSET; d++)
+	for (int iw = OFFSET; iw < img1Info.resDepth-OFFSET; iw++)
 	{
-		for (int w = OFFSET; w < img1Info.resWidth-OFFSET; w++) //percorre imagem pixel //coluna
+		for (int ih = OFFSET; ih < img1Info.resWidth-OFFSET; ih++) //percorre imagem pixel //coluna
 		{
-			for (int h = OFFSET; h < img1Info.resHeight-OFFSET; h++) //a pixel //linha
-			{	
-				if(V)printf("\n[%d,%d]\n",d,ijn(w,h,img1Info.resWidth));
-				buildImagePlanes(d,w,h,img1Info.resWidth);
-				//if(V)printf("**** AXIAL\n");
-				// for(int ii=0; ii < pBase; ii++)
-				// {
-				// 	for(int jj=0; jj < pBase; jj++)
-				// 	{
-				// 		int pData2 = ijn(i-kernel+ii, j-kernel+jj ,img1Info.resWidth);
-				// 		//subImg[ijn(ii, jj, pBase)] = data1[k][pData2];
-				// 		if(V) printf("[%d][%d] ", k, pData2 ); 
-				// 		//printf("%d ", (int)subImg[ijn(ii, jj, pBase)] ); 
-				// 	}
-				// 	if(V)printf("\n");
-				// }
+			for(int ii=0; ii < PBASE; ii++)
+			{
+				for(int jj=0; jj < PBASE; jj++)
+				{			
+					subImg[ijn(ii,jj,PBASE)] = data2[ijn(iw-KERNEL+ii, ih-KERNEL+jj ,img1Info.resWidth)];
+				}
 			}
+			Mat sliceOrig(PBASE,PBASE,CV_8SC1,subImg);
+		
+			for (int vd = OFFSET; vd < img1Info.resDepth-OFFSET; vd++)
+			{
+				for (int vw = OFFSET; vw < img1Info.resWidth-OFFSET; vw++) //percorre imagem pixel //coluna
+				{
+					for (int vh = OFFSET; vh < img1Info.resHeight-OFFSET; vh++) //a pixel //linha
+					{
+						//printf("[%d,%d]\n",d,ijn(w,h,IMG_W));
+						Mat t;
+
+						buidImagePlanes(vd,vw,vh,img1Info.resWidth,data1,0,t);
+            				mpsnrV = qualAssess.getPSNR(sliceOrig,t);
+						if(mpsnrV.val[0] == 0) 	count++;
+						buidImagePlanes(vd,vw,vh,img1Info.resWidth,data1,1,t);
+						mpsnrV = qualAssess.getPSNR(sliceOrig,t);
+						if(mpsnrV.val[0] == 0) 	count++;
+            			buidImagePlanes(vd,vw,vh,img1Info.resWidth,data1,2,t);
+						mpsnrV = qualAssess.getPSNR(sliceOrig,t);
+						if(mpsnrV.val[0] == 0) 	count++;
+            			buidImagePlanes(vd,vw,vh,img1Info.resWidth,data1,3,t);
+						mpsnrV = qualAssess.getPSNR(sliceOrig,t);
+						if(mpsnrV.val[0] == 0) 	count++;
+            			buidImagePlanes(vd,vw,vh,img1Info.resWidth,data1,4,t);
+						mpsnrV = qualAssess.getPSNR(sliceOrig,t);
+						if(mpsnrV.val[0] == 0) 	count++;
+            			buidImagePlanes(vd,vw,vh,img1Info.resWidth,data1,5,t);
+						mpsnrV = qualAssess.getPSNR(sliceOrig,t);
+						if(mpsnrV.val[0] == 0) 	count++;
+            			buidImagePlanes(vd,vw,vh,img1Info.resWidth,data1,6,t);
+						mpsnrV = qualAssess.getPSNR(sliceOrig,t);
+						if(mpsnrV.val[0] == 0) 	count++;
+            			buidImagePlanes(vd,vw,vh,img1Info.resWidth,data1,7,t);
+						mpsnrV = qualAssess.getPSNR(sliceOrig,t);
+						if(mpsnrV.val[0] == 0) 	count++;         
+						buidImagePlanes(vd,vw,vh,img1Info.resWidth,data1,8,t);
+						mpsnrV = qualAssess.getPSNR(sliceOrig,t);
+						if(mpsnrV.val[0] == 0) 	count++;
+            			count3++;
+					}
+				}
+			}
+			count2++;
 		}
-		if(V)printf("################\n");
+		printf("%d\n",iw);
 	}
-
+	//free (subImg);
 	start = clock() - start;
-  	printf ("(%f seconds).\n",((float)start)/CLOCKS_PER_SEC);
-
-
-	return 0;
+  	printf ("(%f seconds).\n",((float)start)/CLOCKS_PER_SEC);	
+	printf("%d,%d,%d\n",count2,count,count3 );
+	return 0;		
 }
